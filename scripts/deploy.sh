cca3ce7956fa8fce703611a47df2a0cd0b2fbb9d
Archit Khode <arkits@outlook.com>
2020-03-11 20:41:15 -0700

    update deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+# deploy_chaddi.sh
+
+# Stop if there is an error
+set -e
+
+# Go to chaddi home
+cd /opt/software/chaddi-api
+
 git reset --hard origin
 
 git pull
--- scripts/run_prod.sh
+++ scripts/run_prod.sh
@@ -2,6 +2,8 @@ cd ..
 
 source .env/bin/activate
 
+pip install -r requirements.txt
+
 cd src
 
 gunicorn -b 0.0.0.0:8789 chaddi_api:app > ../chaddi_api.log 2>&1 & 
