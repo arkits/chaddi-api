e6087380889ca50411efa9b04fe58dbc5eafdd25
Archit Khode <arkits@outlook.com>
2020-03-11 22:00:32 -0700

    revert edits to __init__.py/app

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,4 +1,3 @@
-from app import routes
 from flask import Flask
 from domain import version
 
@@ -8,8 +7,6 @@ import logging
 SERVER_NAME = version.get_version()['name']
 
 # create a custom handler
-
-
 class InterceptHandler(logging.Handler):
     def emit(self, record):
         logger_opt = logger.opt(depth=6, exception=record.exc_info)
@@ -18,7 +15,7 @@ class InterceptHandler(logging.Handler):
 
 class customFlask(Flask):
 
-    # Every response will be processed here first
+    #Every response will be processed here first
     def process_response(self, response):
         response.headers['server'] = SERVER_NAME
         response.headers['Access-Control-Allow-Origin'] = "*"
@@ -26,12 +23,12 @@ class customFlask(Flask):
         response.headers['Access-Control-Request-Headers'] = "*"
         return(response)
 
-
 app = customFlask(__name__)
 
 # logging properties are defined in config.py
-logger.start("log/chaddi-api.log", level="INFO",
-             backtrace=True, rotation='25 MB')
+logger.start("log/chaddi-api.log", level="INFO", backtrace=True, rotation='25 MB')
 
 # register loguru as handler
 app.logger.addHandler(InterceptHandler())
+
+from app import routes
